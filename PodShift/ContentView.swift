c1b5db12271b3ad98e89b428f8aaf101060ba4ad
Pierre-Luc Robitaille <pierreluc2002@gmail.com>
2024-09-28 00:23:54 -0400

    Initial Commit

---
+++ PodShift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PodShift
+//
+//  Created by Pierre-Luc Robitaille on 2024-09-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PodShift/PodShiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodShiftApp.swift
+//  PodShift
+//
+//  Created by Pierre-Luc Robitaille on 2024-09-28.
+//
+
+import SwiftUI
+
+@main
+struct PodShiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
